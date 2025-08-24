6ff8d962203904701c0cd97c27884f50321f85f0
TuNombre <tuemail@example.com>
2025-08-24 14:01:40 -0400

    ci: add GitHub Actions pipeline for PHP & JS

--- backend/vendor/composer/autoload_real.php
+++ backend/vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInit4f47d5993543e534a98da74f38d186d1
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit4f47d5993543e534a98da74f38d186d1::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
